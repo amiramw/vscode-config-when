765e4978627af1d3df26c353e71655d8621d660a
Amiram Wingarten <amiram.wingarten@sap.com>
2019-09-08 15:03:09 +0300

    when

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,14 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloworld-sample" is now active!');
-	throw new Error("error");	
+	let disposable = vscode.commands.registerCommand('openworkflow', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World!');
+	});
+
+	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
